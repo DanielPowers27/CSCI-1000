888b4c67473f50adce13275e98f38511911bf213
daniel <dpowers1@lagrange.edu>
2025-04-02 13:36:20 -0400

    Structure of Class

---
+++ CSCI-1000.py
@@ -0,0 +1,28 @@
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+i = 0
+while i<len(list):
+  print(list[i])
+  i += 1
+
+
+#list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#for y in list:
+#    print(y)
+#print("Loop Complete")
+
+
+#password = input("Enter Password:")
+#if password == "Kangaroo":
+#  print("Welcome Admin!")
+#elif password == "Dog":
+#  print("Welcome Student!")
+#else:
+#  print("Access Denied")
+
+
+#print("Colors of the Rainbow: Red, Orange, Yellow, ???, Blue, Indigo, Violet")
+#color = input("What Color is Missing in this Rainbow? ")
+#if color == "Green" or "green":#I tested the word "or" to see if this would accept both inputs and it worked
+#  print("That's Right!")
+#else:
+#  print("Sorry, that's not right!")
